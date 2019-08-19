ebf3a86a0a97bd874f25a0a2bef7c457b9c343c2
unknown <jjamal95@gmail.com>
2019-08-18 19:29:36 -0500

    finished helper functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,28 +1,69 @@
-const db = require("../data/dbConfig");
-//const db = require("../data/dbConfig") = knex(configOptions);
 
+const db = require('../data/dbConfig');
+
+const find = () => {
+    return db('schemes')
+}
+
+const findById = (id) => {
+    return db('schemes')
+    .where({ id })
+    .first()
+    .then(scheme => {
+        if (scheme) {
+          return scheme;
+        } else {
+          return null;
+        }
+    })
+}
+
+const findSteps = (id) => {
+    return db('schemes')
+    .innerJoin('steps', 'schemes.id', 'steps.scheme_id')
+    .where({ scheme_id: id })
+    .select('schemes.scheme_name', 'step_number', 'instructions' )
+}
+
+const add = (scheme) => {
+    return db('schemes')
+    .insert(scheme, 'id')
+    .then(schemes => {
+        const SchemeID = schemes[0];
+        return SchemeID
+    })
+}
+
+const update = (changes, id) => {
+    return db('schemes').where({ id })
+    .update(changes)
+    .then(changed => {
+        if(changed) {
+            return changed
+        }
+    })
+}
+
+const remove = (id) => {
+    return db('schemes').where({ id })
+    .delete()
+    .then(scheme => {
+        if(scheme) {
+            return scheme
+        } else {
+            return null;
+        }
+    })
+}
 
 
 module.exports = {
     find,
-    findById
-  };
-
-
-
-
-function find() {
-    return db('schemes')
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
 }
-
-
-
-function findById(id) {
-    return db('schemes')
-      .where({ id })
-      .first()
-  
-  }
-
 
 
